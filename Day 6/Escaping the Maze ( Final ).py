6ad028a8a5bc2275fe561812c451d02dcbcf16a1
Burak Gür <54892385+burakguur@users.noreply.github.com>
2023-03-31 03:31:18 +0300

    Update Escaping the Maze ( Final ).py

--- Day 6/Escaping the Maze ( Final ).py
+++ Day 6/Escaping the Maze ( Final ).py
@@ -7,9 +7,12 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
+
+#Unexpected infinite loop
 while front_is_clear():
     move()
 turn_left()
+
 
 while not at_goal():
     if right_is_clear():
