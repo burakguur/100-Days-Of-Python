d419c88900cfc7641be8cb06b1fb4aa84bba1b0e
Burak Gür <burakyigitgur@ogr.iu.edu.tr>
2023-04-04 06:09:15 +0300

    Delete Band Name Generator.py

--- Day 1/Band Name Generator.py
+++
@@ -1,5 +0,0 @@
-print("***Welcome to Band Name Generator***")
-street = input("What is the name of the street where you grew up?")
-pet = input("What is pet's name ? ")
-print("Your Bande Name can be " + street +" "+ pet)
-
